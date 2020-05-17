5a5bdfd9d78538bebd8c4c9a7ab29ceb804805a1
amit666hol <36861082+amit666hol@users.noreply.github.com>
2020-05-17 14:43:11 +0300

    add size variability to the nn

--- sansml.py
+++ sansml.py
@@ -15,8 +15,10 @@ class Net(nn.Module):
     width = 640
     num_classes = 5
 
-    def __init__(self):
+    def __init__(self, width, height):
         super(Net, self).__init__()
+        self.width = width
+        self.height = height
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
 
@@ -30,14 +32,14 @@ class Net(nn.Module):
         # self.relu1 = nn.ReLU()
         # self.relu2 = nn.ReLU()
         # self.fc = nn.Linear(in_features=320 * 240 * 16, out_features=self.num_classes)
-        self.fc1 = nn.Linear(in_features=307200 , out_features=120)
+        self.fc1 = nn.Linear(in_features=self.width * self.height, out_features=120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 5)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x.float())))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 307200 )
+        x = x.view(-1, self.width * self.height)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -62,24 +64,30 @@ classes = ['up', 'down', 'left', 'right', 'none']
 
 
 class DataSet(object):
-    def __init__(self, root):
+    def __init__(self, root, width, height):
         self.root = root
         # load all image files, sorting them to
         # ensure that they are aligned
         self.imgs = list(sorted(os.listdir(os.path.join("", self.root))))
+        self.width, self.height = width, height
 
     def __getitem__(self, idx):
         # load images
         img_path = os.path.join("", self.root, self.imgs[idx])
         img = cv2.imread(img_path)
         try:
+            img = cv2.resize(img, (self.width, self.height))
             img = np.moveaxis(img, -1, 0)
         except Exception as e:
             print(img_path)
             raise e
         img_label = img_path.split("\\")[1]
         img_label = img_label.split(".")[0]
-        img_label = ''.join([i for i in img_label if not i.isdigit()])
+        for i in range(len(img_label)):
+            if img_label[i].isdigit():
+                img_label = img_label[:i]
+                break
+        # img_label = ''.join([i for i in img_label if not i.isdigit()])
         img_label = classes.index(img_label)
         # print("Label: " + img_label + " Index: " + str(idx))
         # there is only one class
@@ -93,25 +101,20 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 
 
-def main():
+def main(root):
     # get some random training images
-    transform = transforms.Compose([transforms.ToTensor()]) # Defing PyTorch Transform
+    # transform = transforms.Compose([transforms.ToTensor()]) # Defing PyTorch Transform
+    width, height = int(640/8), int(480/8)
 
-
-    trainset = DataSet("backup")
-    testset = DataSet("backup")
+    trainset = DataSet(root, width, height)
 
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                               shuffle=True, num_workers=2)
-
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=2)
-
-    net = Net()
+    net = Net(width, height)
     VERY_NICE_criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.00001, momentum=0.9)
 
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(3):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -129,25 +132,47 @@ def main():
 
             # print statistics
             running_loss += loss.item()
-            if i % 10 == 9:  # print every 2000 mini-batches
+            freq = 20
+            if i % freq == freq-1:  # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss))
                 running_loss = 0.0
 
     print('Finished Training')
-    PATH = "sans_model.pth"
+    PATH = "sans_model6.pth"
     torch.save(net.state_dict(), PATH)
 
-def AAAAAAA():
-    root = "C:\\Users\\1\PycharmProjects\SERVER\ML\sans_ml\images\\"
-    imgs = list(os.listdir("images"))
-    i = 0
+# def AAAAAAA():
+#     root = "C:\\Users\\1\PycharmProjects\SERVER\ML\sans_ml\images\\"
+#     imgs = list(os.listdir("images"))
+#     i = 0
+#     for image in imgs:
+#         img = cv2.imread(root + image)
+#         img = cv2.resize(img, (32, 32))
+#         cv2.imwrite(root + image, img)
+#         print(i * 100 / len(imgs))
+#         i += 1
+
+
+def renameFile(root):
+    imgs = list(os.listdir(root))
     for image in imgs:
-        img = cv2.imread(root + image)
-        img = cv2.resize(img, (32, 32))
-        cv2.imwrite(root + image, img)
-        print(i * 100 / len(imgs))
-        i += 1
+        dot = image.split(".")
+        before_dot = dot[0]
+        after_dot = dot[1]
+        os.rename(root + image, root + before_dot + "a." + after_dot)
+
+
+def color_filter(root):
+    imgs = list(os.listdir(root))
+    min_YCrCb = np.array([0, 133, 77], np.uint8)
+    max_YCrCb = np.array([235, 173, 127], np.uint8)
+    for image in imgs:
+        frame = cv2.imread(root + image)
+        image_YCrCb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)  # Convert the frame from RGB to YCrCb
+        skinRegionYCrCb = cv2.inRange(image_YCrCb, min_YCrCb, max_YCrCb)
+        skinYCrCb = cv2.bitwise_and(frame, frame, mask=skinRegionYCrCb)
+        cv2.imwrite("images5\\" + image, skinYCrCb)
 
 def test():
     import random
@@ -158,7 +183,7 @@ def test():
     PATH = "sans_model.pth"
     net.load_state_dict(torch.load(PATH))
     for i in range(20):
-        image, label = data[random.randint(0, 1252)]
+        image, label = data[random.randint(0, len(data))]
         output = net(torch.tensor([image]))
         _, predicted = torch.max(output, 1)
         predicted = predicted[0].item()
@@ -167,6 +192,8 @@ def test():
 
 
 if __name__ == '__main__':
-    # main()
+    main("images5")
     # AAAAAAA()
-    test()
+    # test()
+    # renameFile("images3\\")
+    # color_filter("images4\\")
